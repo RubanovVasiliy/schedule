a868484c5f0dc96b47b2d27872789f9f6f5118b5
Rubanov Vasiliy <rubanovas2005@gmail.com>
2023-04-18 21:29:14 +0700

    allocated the parser logic to a new class

--- ReactAsp/Controllers/UploadController.cs
+++ ReactAsp/Controllers/UploadController.cs
@@ -1,9 +1,8 @@
-﻿using System.Text;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using OfficeOpenXml;
+using ReactAsp.Utils;
 
 namespace ReactAsp.Controllers;
-
 
 [ApiController]
 [Route("[controller]")]
@@ -18,72 +17,34 @@ public class UploadController : ControllerBase
             {
                 return BadRequest(new { message = "File not found" });
             }
-            
+
             var file = Request.Form.Files[0];
-            
+
             if (file.ContentType != "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
             {
                 return BadRequest(new { message = "Invalid file format. Please upload an Excel file" });
             }
-            
+
             var filePath = Path.GetTempFileName();
 
             await using (var stream = new FileStream(filePath, FileMode.Create))
             {
                 await file.CopyToAsync(stream);
             }
-            
+
             var fileInfo = new FileInfo(filePath);
-            //if (!fileInfo.Exists) return Ok(new { message = "File uploaded successfully" });
 
             using var package = new ExcelPackage(fileInfo);
             var worksheet = package.Workbook.Worksheets["TDSheet"];
-            var str = new StringBuilder();
-            for (var col = 3; col <= worksheet.Dimension.End.Column; col++)
-            {
-                Console.Write(worksheet.Cells[2, col].Text + "\n");
 
-                for (var row = 3; row <= worksheet.Dimension.End.Row; row++)
-                {
-                    var excelRange = worksheet.Cells[row, col];
-                    var temp = excelRange.Text;
+            var parser = new ExcelParser(worksheet);
+            var result = parser.ParseData();
 
-                    if (string.IsNullOrEmpty(temp)) continue;
+            Console.WriteLine(result);
 
-                    var isOdd = Convert.ToInt16(excelRange.Address[^1]) % 2 == 0;
-
-                    var dayRow = row / 14 * 14 + 4;
-                    var partDay = worksheet.Cells[dayRow, 1];
-
-                    var timeRow = row % 2 == 0 ? row : row - 1;
-                    var partTime = worksheet.Cells[timeRow, 2];
-
-
-                    var times = partTime.Text.Split(" - ");
-                    var startTime = times[0];
-                    var endTime = times[1];
-
-                    var value = string.Join("#", excelRange.Text.Split("\n"));
-                    str.Append(partDay.Text);
-                    str.Append('#');
-                    str.Append(startTime);
-                    str.Append('#');
-                    str.Append(endTime);
-                    str.Append('#');
-                    str.Append(value);
-                    str.Append('#');
-                    str.Append(isOdd ? "1" : "2");
-                    Console.WriteLine(str);
-                    str.Clear();
-                }
- 
-                Console.WriteLine();
-            }
-            package.Dispose();
-            
             return Ok(new { message = "File uploaded successfully" });
         }
-        
+
         catch (Exception ex)
         {
             return BadRequest(new { message = $"Error: {ex.Message}" });
---
+++ ReactAsp/Utils/ExcelParser.cs
@@ -0,0 +1,58 @@
+using System.Text;
+using OfficeOpenXml;
+
+namespace ReactAsp.Utils;
+
+public class ExcelParser
+{
+    private readonly ExcelWorksheet _worksheet;
+
+    public ExcelParser(ExcelWorksheet worksheet)
+    {
+        _worksheet = worksheet;
+    }
+
+    public string ParseData()
+    {
+        var str = new StringBuilder();
+
+        for (var col = 3; col <= _worksheet.Dimension.End.Column; col++)
+        {
+            str.Append(_worksheet.Cells[2, col].Text + "\n");
+
+            for (var row = 3; row <= _worksheet.Dimension.End.Row; row++)
+            {
+                var excelRange = _worksheet.Cells[row, col];
+                var temp = excelRange.Text;
+
+                if (string.IsNullOrEmpty(temp)) continue;
+
+                var isOdd = Convert.ToInt16(excelRange.Address[^1]) % 2 == 0;
+
+                var dayRow = row / 14 * 14 + 4;
+                var partDay = _worksheet.Cells[dayRow, 1];
+
+                var timeRow = row % 2 == 0 ? row : row - 1;
+                var partTime = _worksheet.Cells[timeRow, 2];
+
+                var times = partTime.Text.Split(" - ");
+                var startTime = times[0];
+                var endTime = times[1];
+
+                var value = string.Join("#", excelRange.Text.Split("\n"));
+                str.Append(partDay.Text);
+                str.Append('#');
+                str.Append(startTime);
+                str.Append('#');
+                str.Append(endTime);
+                str.Append('#');
+                str.Append(value);
+                str.Append('#');
+                str.Append(isOdd ? "1" : "2");
+                str.Append('\n');
+            }
+        }
+
+        return str.ToString();
+    }
+}
