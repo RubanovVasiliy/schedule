b69fe6a8e3dd346e0e7170e768ba5d5ca2fbd297
Rubanov Vasiliy <rubanovas2005@gmail.com>
2023-05-09 16:02:12 +0700

    fix

--- ReactAsp/ClientApp/src/index.tsx
+++ ReactAsp/ClientApp/src/index.tsx
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration.js';
 import App from "./App";
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
